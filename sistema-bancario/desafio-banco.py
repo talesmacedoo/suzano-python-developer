19897039b86c330ba9f6a56f10a4753a18ba6e07
Tales Macedo <talesmgbahia@hotmail.com>
2025-08-12 16:40:36 -0300

    criando operacao de deposito

--- sistema-bancario/desafio-banco.py
+++ sistema-bancario/desafio-banco.py
@@ -18,10 +18,15 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-
+        valor = float(input("Digite o valor do deposito:"))
+        if valor > 0:
+            saldo += valor
+            print("Deposito realizado com sucesso!")
+        else: 
+            print("Valor digitado é menor ou igual a zero")
 
     elif opcao == "s":
-
+        pass
 
     elif opcao == "e":
         print("\n================ EXTRATO ================")
