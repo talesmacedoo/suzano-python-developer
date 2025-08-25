cbff91026c00a658dff0136d074e50fea6b85458
Tales Macedo <talesmgbahia@hotmail.com>
2025-08-25 16:10:28 -0300

    refactor: adicionar propriedades cpf e nome na classe PessoaFisica

--- sistema_bancario_v3/models/cliente.py
+++ sistema_bancario_v3/models/cliente.py
@@ -4,6 +4,10 @@ class Cliente():
         self._endereco = endereco
         self._contas = []
 
+    @property
+    def contas(self):
+        return self._contas
+
     def realizar_transacao(self, conta, transacao):
         transacao.registrar(conta)
 
@@ -11,8 +15,16 @@ class Cliente():
         self._contas.append(conta)
 
 class PessoaFisica(Cliente):
-    def __init__(self, endereco, contas, cpf: str, nome: str, data_nascimento: str):
-        super().__init__(endereco, contas)
+    def __init__(self, endereco, cpf: str, nome: str, data_nascimento: str):
+        super().__init__(endereco)
         self._cpf = cpf
         self._nome = nome
         self._data_nascimento = data_nascimento
+
+    @property
+    def cpf(self):
+        return self._cpf
+    
+    @property
+    def nome(self):
+        return self._nome
