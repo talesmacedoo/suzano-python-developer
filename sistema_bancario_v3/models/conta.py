1b927fb3f3fb7e34c09ae219668187d5b0b06383
Tales Macedo <talesmgbahia@hotmail.com>
2025-08-25 13:10:49 -0300

    feat(models): criar método sacar da classe ContaCorrente

--- sistema_bancario_v3/models/conta.py
+++ sistema_bancario_v3/models/conta.py
@@ -60,3 +60,29 @@ class ContaCorrente(Conta):
         super().__init__(saldo, numero, agencia, Cliente, Historico)
         self._limite = limite
         self._limite_saques = limite_saques
+
+    def sacar(self, valor):
+        numero_saques = len(
+            [transacao for transacao in self.historico.transacoes if transacao["tipo"] == Saque.__name__]
+        )
+
+        excedeu_limite = valor > self._limite
+        excedeu_saques = numero_saques >= self._limite_saques
+
+        if excedeu_limite:
+            print("\nOperação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("\nOperação falhou! Número máximo de saques excedido.")
+
+        else:
+            return super().sacar(valor)
+
+        return False
+    
+    def __str__(self):
+        return f"""\
+            Agência:\t{self.agencia}
+            C/C:\t\t{self.numero}
+            Titular:\t{self.cliente.nome}
+        """
